70566f6affb7c66d03c1daaa9a591552856b0b85
오정민 <arttjsal@gmail.com>
2026-06-28 21:46:45 +0900

    Update step4_esp32_page.ino

--- esp32-class-projects/attendance/step4_esp32_page/step4_esp32_page.ino
+++ esp32-class-projects/attendance/step4_esp32_page/step4_esp32_page.ino
@@ -11,6 +11,8 @@
 
 #include <WiFi.h>
 #include <WebServer.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 
 // ESP32가 직접 만들 와이파이 이름/비밀번호
 const char* AP_SSID = "ESP32-Attend";   // 폰 와이파이 목록에 뜰 이름
@@ -53,6 +55,7 @@ void handleRoot() {
 }
 
 void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   Serial.begin(115200);
   delay(300);
 
